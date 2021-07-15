27f239cd9262162c30c2ec785ed99e90105929ce
Nana <tsampaio21@gmail.com>
2021-07-15 17:16:19 -0300

    Renderização de listas

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import Post from './components/Post/Post';
 
 const MainContainer = styled.div`
@@ -9,11 +9,71 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
-class App extends React.Component {
+const Foemulario = styled.div`
+  border: 1px solid black;
+  margin: 16px;
+  padding: 16px;
+
+`
+
+ class App extends React.Component{
+  state ={
+    post: [ {  
+
+    nomeUsuario: "",
+    fotoUsuario: "",
+    fotoPost: ""
+  }
+ ],
+
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+};
+
+adicionaPost = () =>{
+  const novoPost = {
+    nomeUsuario: this.state.valorInputNomeUsuario,
+    fotoUsuario: this.state.valorInputFotoUsuario,
+    fotoPost: this.state.valorInputFotoPost
+  };
+
+ const novoPosts = [...this.state.post, novoPost];
+
+  this.setstate({ post: novoPost,
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+
+  });
+
+};
+
+onChangeInputNomeUsuario = (event) => {
+    this.setState({valorInputNomeUsuario: event.target.value});
+};
+
+onChangeInputFotoUsuario = (event) =>{
+  this.setState({valorInputFotoUsuario: event.target.value});
+};
+
+onChangeInputFotoPost = (event) =>{
+  this.setState({valorInputFotoPost: event.target.value});
+};
+    
   render() {
+    const listaDeComponentes = this.state.post.map((post) => {
+      return (
+        <p>
+          {post.nomeUsuario} - {post.fotoUsuario}- {post.fotoPost}
+        </p>
+      );
+    });
+
+
     return (
       <MainContainer>
-        <Post
+         <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
@@ -31,6 +91,30 @@ class App extends React.Component {
           fotoPost={'https://cdn.pixabay.com/photo/2015/05/26/04/20/landscape-784200_960_720.jpg'}
         />
 
+        <div>
+
+        <input
+         value = {this.state.valorInputNomeUsuario}
+         onChange ={ this.onChangeInputNomeUsuario}
+         placeholder = {"Nome"}
+        />
+
+        <input
+           value = {this.state.valorInputFotoUsuario}
+           onChange ={ this.onChangeInputFotoUsuario}
+           placeholder = {"Foto usuário"}
+        />
+
+        <input
+           value = {this.state.valorInputFotoPost}
+           onChange ={ this.onChangeInputFotoPost}
+           placeholder = {"Foto post"}
+        />
+
+        </div>
+        <button onClick = {this.adicionaPost} > Adicionar</button>
+
+        <div>{listaDeComponentes}</div>
       </MainContainer>
     );
   }
