aa5f7272071cff5dc2f625643d886cbf7d9ddc2c
Nana <tsampaio21@gmail.com>
2021-07-19 18:59:46 -0300

    exercício renderização condicional

---
+++ semana6/formlab/src/components/Etapa1.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+
+
+class Etapa1 extends Component {
+    render(){
+        return(
+            <div>
+                <h1>ETAPA1 - DADOS GERAIS</h1> 
+                
+                <h2>1. Qual o seu nome?</h2> 
+                <input></input>
+
+                <h2>2. Qual a sua idade?</h2> 
+                <input></input>
+
+                <h2>3. Qual o seu email?</h2> 
+                <input></input>
+
+                <h2>4. Qual a sua escolaridade?</h2> 
+                <input></input>
+
+                
+            </div>
+        )
+    }
+}
+
+
+export default Etapa1;
---
+++ semana6/formlab/src/components/Etapa3.js
@@ -0,0 +1,27 @@
+
+
+import React, { Component } from 'react';
+
+
+
+
+class Etapa3 extends Component {
+    render(){
+        return(
+            <div>
+                 <h1>ETAPA3 - INFORMAÇÕES GERAIS DE ENSINO </h1> 
+                
+
+                <h2>5. Por que você não terminou o curso de graduação?</h2> 
+                <input></input>
+
+                <h2>6. Você fez algum curso complementar?</h2> 
+                <input></input>
+            </div>
+        )
+    }
+}
+
+
+
+export default Etapa3;
