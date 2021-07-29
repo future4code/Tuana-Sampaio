f87cd898f49e9674f32f788f8af22914b00892db
Nana <tsampaio21@gmail.com>
2021-07-29 17:05:14 -0300

    Iniciando o projeto

---
+++ semana7/apis-na-natureza/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from "axios"
+
+
+
+
+class App extends React.component {
+  render(){
+    return (
+      <div>
+        <p>OLá</p>
+      </div>
+    )
+  }
+  
+}
+
+export default App;
