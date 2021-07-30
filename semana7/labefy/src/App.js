b6d61d2ec57d2c8b4eccc5f343c524a6cd06b8af
Nana <tsampaio21@gmail.com>
2021-07-30 18:35:30 -0300

    adição dos botões para informações e adicionar músicas

--- semana7/labefy/src/App.js
+++ semana7/labefy/src/App.js
@@ -19,8 +19,13 @@ const headers = {
 class App extends React.Component{
 
   state = {
+    //telaAtual
     playlists: [],
-    inputValueName: ""
+    inputValueName: "",
+    tracks: [],
+    inputValueNameTrack: "",
+    inputValueNameArtist: "",
+    inputValueUrl:""
   }
 
 
@@ -28,6 +33,7 @@ class App extends React.Component{
 componentDidMount() {
   this.getAllPlaylists()
   this.getPlaylistTrack()
+  this.addTrackToPlaylist()
  
 }
 
@@ -78,26 +84,60 @@ componentDidMount() {
     })
   }
 
-  addTrackToPlaylist = () => {
+  //adiciona uma música a uma playlist existente
+  addTrackToPlaylist = (id) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
+    const body = {
+      "name": this.state.inputValueNameTrack, 
+      "artist": this.state.inputValueNameArtist,
+      "url": this.state.inputValueUrl
+    }
+    axios.post(url, body, headers).then((res)=> {
+      console.log(res.data.result)
+      this.getAllPlaylists()
+      
+    }).catch((err)=> {
+      console.log(err)
+    })
     
   }
 
+  // inputs para adicionar as musicas
+  onChangeinputValueNameTrack = (event) => {
+    this.setState({inputValueNameTrack: event.target.value} )
+  }
+  onChangeinputValueNameArtist = (event) => {
+    this.setState({inputValueNameArtist: event.target.value} )
+  }
+  onChangeinputValueUrl = (event) => {
+    this.setState({inputValueUrl: event.target.value} )
+  }
+
+  //verificar quais músicas estão em uma determinada playlist
   getPlaylistTrack = (id) => {
-    const urltrack = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
-    axios.get(urltrack, headers)
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
+    axios.get(url, headers)
     .then((res)=> {
+      alert("tudo certo")
       console.log(res.data)
     }).catch((err)=> {
-      console.log(err)
+      alert(err.response.data.result)
+      console.log(err.response)
     })
   }
 
   render() {
     const conjuntoPlaylists = this.state.playlists.map((playlist) => {
-      return <li > <option key = {playlist.id} >{playlist.name} </option>
+      return <li key = {playlist.id}> <option  >{playlist.name} {playlist.id} </option>
       <button onClick = {() => this.deletePlaylist(playlist.id)}>delete</button>
+      <button onClick = {() => this.getPlaylistTrack(playlist.id)}>Informações da playlist</button>
+      <button onClick = {() => this.addTrackToPlaylist(playlist.id)} >Adicionar música</button>
             </li>
       
+    })
+
+    const conjuntoTracks = this.state.tracks.map((musica)=> {
+      return <li> {musica.name} </li>
     })
   
     return (
@@ -112,9 +152,29 @@ componentDidMount() {
         
         />
         <button onClick = {this.createPlaylist} >Adicionar playlist</button>
-        <select>{conjuntoPlaylists}</select>
+        <select></select>
         
         
+
+        <input
+        placeholder = "Nome da música"
+        value = {this.state.inputValueNameTrack}
+        onChange = {this.onChangeinputValueNameTrack}
+        />
+
+        <input
+        placeholder = "Nome do artista"
+        value = {this.state.inputValueNameArtist}
+        onChange = {this.onChangeinputValueNameArtist}
+        />
+
+        <input
+        placeholder = "url da música"
+        value = {this.state.inputValueUrl}
+        onChange = {this.onChangeinputValueUrl}
+        />  
+        
+       {conjuntoPlaylists}
       </div>
     );
   }
