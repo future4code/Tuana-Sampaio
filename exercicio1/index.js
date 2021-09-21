76a1753a8d7713693257c584ab13e2451e678a96
Nana <tsampaio21@gmail.com>
2021-09-20 21:48:31 -0300

    resolução exercicios 1, 2 e 3 de node.js

---
+++ exercicio1/index.js
@@ -0,0 +1,11 @@
+/*a) Como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+    R-  Acessamos através do process.argv. Exemplo: const input = process.argv[2]*/
+
+/*b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:*/
+    const{red}= require("chalk")
+
+    let nome = "Tuana"
+    let idade = Number(31)
+    let novaIdade = idade + Number(7)
+
+    console.log(red(`Olá, ${nome} ! Você tem ${idade} anos.Em sete anos você terá ${novaIdade} anos. :(`))
---
+++ exercicio3/index.js
@@ -0,0 +1,8 @@
+
+
+    const tarefa = ["comprar pão"]
+    const novaTarefa = process.argv[2]
+    let listaDeTarefas = tarefa.push(novaTarefa) 
+
+console.log(`${tarefa}`)
+
